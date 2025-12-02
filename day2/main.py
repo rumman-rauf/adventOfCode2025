6ac97fdf689c06c5d4411bf7d7a73e8ac9e1801d
Rumman Rauf <rumman15rauf@gmail.com>
2025-12-03 00:18:01 +1100

    fuck u

--- day2/main.py
+++ day2/main.py
@@ -1,2 +1,18 @@
-with open('day2/input.txt') as f:
+with open('input.txt') as f:
+    pairs = [tuple(map(int,pair.split('-'))) for pair in f.read().strip().split(",")]
+
+def partOne():    
+    r = 0
+    for pair in pairs:
+        for n in range(pair[0], pair[1] + 1):
+            x = str(n)
+            if len(x) % 2 != 0:
+                continue
+            if x[:len(x) // 2] == x[len(x) // 2:]:
+                r += n
+    return r
+
+def partTwo():
     pass
+
+print("part one:", partOne())
