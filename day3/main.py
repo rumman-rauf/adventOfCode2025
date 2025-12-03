948b0b72c4b46309324a059ce0fdd2949cfeb6c3
Rumman Rauf <rumman15rauf@gmail.com>
2025-12-03 23:31:31 +1100

    day3

--- day3/main.py
+++ day3/main.py
@@ -1,26 +1,30 @@
-with open("input.txt") as f:
-    lines = f.readlines()
-
-def joltage(line: str) -> int:
+def joltage(line: str, batteries_needed: int) -> int:
     line = line.strip()
 
-    largest = -1
-    second_largest = -1
-    for digit in line[:-1]:
-        digit = int(digit)
-        if digit > largest:
-            largest = digit
-            second_largest = -1
-        elif digit > second_largest:
-            second_largest = digit
+    largest_digits = [-1] * batteries_needed
+    for i in range(len(line)):
+        digit = int(line[i])
+        found_larger_preceding_digit = False
 
-    if int(line[-1]) > second_largest:
-        second_largest = int(line[-1])
+        x = 0
+        if len(line) <= i + batteries_needed:
+            x = i + batteries_needed - len(line)
+        for j in range(x, len(largest_digits)):
+            if (not found_larger_preceding_digit) and digit > largest_digits[j]:
+                largest_digits[j] = digit
+                found_larger_preceding_digit = True
+            elif found_larger_preceding_digit:
+                largest_digits[j] = -1
 
-    return largest * 10 + second_largest
+    return int(''.join((str(digit) for digit in largest_digits)))
 
-def partOne():
-    total = sum((joltage(line) for line in lines))
-    print(total)
+def part_one(lines: list[str]):
+    return sum((joltage(line, 2) for line in lines))
 
-partOne()
+def part_two(lines: list[str]):
+    return sum((joltage(line, 12) for line in lines))
+
+with open("input.txt") as f:
+    lines = f.readlines()
+print("part 1:", part_one(lines))
+print("part 2:", part_two(lines))
